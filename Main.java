677f38a279fb63e809659c0a04f1c3b16538eee2
andrik-150 <andrik-150@github.com>
2023-10-11 08:23:34 +0300

    task

---
+++ Main.java
@@ -0,0 +1,14 @@
+package seminar2
+        ;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("УАЗ", "Патриот", 2020);
+        System.out.println(car);
+
+        Motorcycle moto = new Motorcycle("Ирбитский Мотоциклетный Завод", "Урал", 2010);
+        System.out.println(moto);
+    }
+
+}
